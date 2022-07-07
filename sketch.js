ca5c93c8bfef348b7f060b8c4d8b11bd0efb1e31
KrishivP <108347370+KrishivP@users.noreply.github.com>
2022-07-07 20:06:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var runner, road, invisibleBoundary1, invisibleBoundary2, roadImage, runnerAnimation;
+
+function preload(){
+  //pre-load images
+  roadImage = loadImage("path.png");
+  runnerAnimation = loadAnimation("Runner-1.png", "Runner-2.png"); 
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+  road = createSprite(200, 200);
+  road.addImage(roadImage);
+  invisibleBoundary1 = createSprite(35, 200, 70, 400);
+  invisibleBoundary1.visible = false;
+  invisibleBoundary2 = createSprite(365, 200, 70, 400);
+  invisibleBoundary2.visible = false;
+  runner = createSprite(305, 350, 25, 100);
+  runner.addAnimation("running", runnerAnimation);
+  runner.scale = 0.05;
+}
+
+function draw() {
+  background(0);
+
+  //creating an infinite road
+  road.velocityY = 10;
+  if (road.y > 450) {
+    road.y = height/2;
+  }
+
+  //setting the x pos of runner to x pos of mouse
+  runner.x = mouseX;
+
+  //making the runner collide with the invisible boundary
+  runner.collide(invisibleBoundary1);
+  runner.collide(invisibleBoundary2);
+
+  //fixing bugs
+  if (mouseX < 111.675) {
+    runner.x = 111.675;
+  }
+  if (mouseX > 288.324) {
+    runner.x = 288.324;
+  }
+
+  drawSprites();
+}
+
+
